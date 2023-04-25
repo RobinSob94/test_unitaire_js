c14a174ee3398a523b1c2c73f7fe43db337faebf
Robin Sobasto <robin.sobasto94@gmail.com>
2023-04-25 18:08:32 +0200

    test

--- exercice_2_1/user.test.js
+++ exercice_2_1/user.test.js
@@ -1,7 +1,7 @@
 const User = require('./user');
 
 test('je test mon user', ()=>{
-    const user = new User("robin.sobasto@gmail.com","Sobasto", "Robin","1997-02-09");
+    const user = new User("robinn.sobasto@gmail.com","Sobasto", "Robin","1997-02-09");
 
     user.isValid();
     expect(user.isValid()).toBeTruthy();
